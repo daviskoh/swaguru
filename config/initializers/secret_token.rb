3ea93fdeb7f6fdff53fc2c60eb71b0577f783af4
daviskoh <koh.davis.0@gmail.com>
2013-12-13 11:49:06 -0500

    yml & dependencies

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swaguru::Application.config.secret_key_base = '1c2f69a5225a9bd467b46f9556d0c71f38a161bac58fd1e0d8786c3786363d3cd66cd41296c591e3ffb3772b3e344eb6a5194d1f04dd5edd60ce95e575d64bf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Swaguru::Application.config.session_store :cookie_store, key: '_swaguru_session'
