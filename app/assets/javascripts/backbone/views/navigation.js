603762f488eba8b05b664703f7250f32b6de1174
daviskoh <koh.davis.0@gmail.com>
2013-12-16 13:36:13 -0500

    functioning backbone nav links

--- app/assets/javascripts/backbone/views/navigation.js
+++ app/assets/javascripts/backbone/views/navigation.js
@@ -3,7 +3,9 @@ var NavigationView = Backbone.View.extend({
   className: 'fixed',
 
   events: {
-    'click a#home-nav': 'goToHome'
+    'click a#home-nav': 'goToHome',
+    'click a#sign-up-nav': 'goToSignUp',
+    'click a#sign-in-nav': 'goToSignIn'
   },
 
   template: _.template($("script.navigation[type='text/html']").html()),
@@ -14,8 +16,19 @@ var NavigationView = Backbone.View.extend({
     return this;
   },
 
-  goToHome: function(){
+  goToHome: function(e){
+    e.preventDefault();
+    Backbone.history.navigate('', {trigger: true});
+  },
 
+  goToSignUp: function(e) {
+    e.preventDefault();
+    Backbone.history.navigate('users/new', {trigger: true})
+  },
+
+  goToSignIn: function(e) {
+    e.preventDefault();
+    Backbone.history.navigate('session/new', {trigger: true})
   },
 
   render: function() {
