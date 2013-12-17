65a5e633b0f850c7e96e0a7ec74df54606a21b27
daviskoh <koh.davis.0@gmail.com>
2013-12-17 17:24:14 -0500

    hacky solution: user can now sign back in after signing out; poor memory alloc

--- app/assets/javascripts/backbone/views/user_show.js
+++ app/assets/javascripts/backbone/views/user_show.js
@@ -3,7 +3,7 @@ var UserShowView = Backbone.View.extend({
   className: 'user-show-main-container',
 
   events: {
-    'click a img': function(e){e.preventDefault();}
+    'click a img': 'addImage'
   },
 
   template: _.template($("script.user-show-page[type='text/html']").html()),
@@ -15,6 +15,14 @@ var UserShowView = Backbone.View.extend({
 
     this.render();
     return this;
+  },
+
+  addImage: function(e) {
+    e.preventDefault();
+
+    // var photo = ;
+
+    // this.model.save({profile_photo: })
   },
 
   render: function() {
