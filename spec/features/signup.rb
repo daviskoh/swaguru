9ca2664bdabf51aa85021e634889ce0dca9023dc
daviskoh <koh.davis.0@gmail.com>
2014-01-03 00:08:30 -0500

    need to fix pushstate before further acceptance testing

--- spec/features/signup.rb
+++ spec/features/signup.rb
@@ -2,19 +2,18 @@ require 'spec_helper'
 
 describe 'a user can create an account', js: true do 
   it 'creates a new user' do
-    visit '/#users/new'
+    visit '/users/new'
 
     within "form.new-user" do
-      fill_in :name, with: "Bob Homie"
-      fill_in :email, with: "Bob Homie"
-      fill_in :password, with: "Bob Homie"
+      fill_in :name, with: "Bob"
+      fill_in :email, with: "bob@bob.com"
+      fill_in :password, with: "Bob"
+      fill_in :password_confirmation, with: "Bob"
       select 'male', from: 'gender'
 
       click_button "Sign Up"
     end
 
-    within "form.sign-in" do
-      expect(page).to have_content "Created new account!"
-    end
+    expect(User.last.name).to eq('Bob')
   end
 end
