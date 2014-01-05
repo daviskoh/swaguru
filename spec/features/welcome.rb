59018b1df84f062c0594013143b44064786f025e
daviskoh <koh.davis.0@gmail.com>
2014-01-05 15:26:35 -0500

    signup option test

--- spec/features/welcome.rb
+++ spec/features/welcome.rb
@@ -1,2 +1,23 @@
 require 'spec_helper'
 
+describe 'a user can choose between signup & login', js: true do 
+  it 'allows for signup' do 
+    visit '/'
+
+    within 'div.container1' do 
+      click_link 'Sign Up'
+    end
+
+    within 'div.container1' do 
+      ['name', 'email', 'password', 'password_confirmation'].each do |field|
+        expect(page).to have_field field
+      end
+
+      expect(page).to have_select 'gender'
+    end
+  end
+
+  it 'allows for login' do 
+    visit '/'
+  end
+end
