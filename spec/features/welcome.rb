147c4764168f2fd556b141bd1a7ba3e7015efc5f
daviskoh <koh.davis.0@gmail.com>
2014-01-05 15:47:20 -0500

    welcome page tests passed

--- spec/features/welcome.rb
+++ spec/features/welcome.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe 'a user can choose between signup & login', js: true do 
-  it 'allows for signup' do 
+  it 'welcomes the user' do 
+    visit '/'
+
+    within 'div.container1' do 
+      expect(page).to have_content 'Welcome to Swagu.ru'
+    end
+  end
+
+  it 'allows Sign Up' do 
     visit '/'
 
     within 'div.container1' do 
@@ -9,15 +17,37 @@ describe 'a user can choose between signup & login', js: true do
     end
 
     within 'div.container1' do 
+      expect(page).to have_content 'Create a New Account'
+    end
+
+    within 'form.new-user' do 
       ['name', 'email', 'password', 'password_confirmation'].each do |field|
         expect(page).to have_field field
       end
 
       expect(page).to have_select 'gender'
+
+      expect(page).to have_button 'Sign Up'
     end
   end
 
-  it 'allows for login' do 
+  it 'allows for Sign In' do 
     visit '/'
+
+    within 'div.container1' do 
+      click_link 'Sign In'
+    end
+
+    within 'div.container1' do 
+      expect(page).to have_content 'Sign In'
+    end
+
+    within 'form.sign-in' do 
+      ['email', 'password'].each do |field|
+        expect(page).to have_field field
+      end
+
+      expect(page).to have_button 'Sign In'
+    end
   end
 end
