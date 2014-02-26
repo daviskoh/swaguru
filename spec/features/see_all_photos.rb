b30037d1304c7aa0f567d6ceb6f62a7acfe9434e
daviskoh <koh.davis.0@gmail.com>
2014-02-25 22:00:08 -0500

    write & prep spec

--- spec/features/see_all_photos.rb
+++ spec/features/see_all_photos.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe 'user can see all posted photos', js: true do 
-  it "displays all other users' photos" do
-    
+  before :each do
+    # visit /users
   end
+
+  it "displays all other users' photos" do
+    # should see ul w/ count === Photo.all.count
+  end
+
+  it "orders photos from newest to oldest"
 end
   
